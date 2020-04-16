06d351cbb6195cec5cdc7762932187ac08892d9b
Andres Posada Llano <posadallano2@gmail.com>
2020-04-16 17:43:14 -0500

    Search component

---
+++ src/utils/colors.js
@@ -0,0 +1,9 @@
+export const colors = {
+    white: "#fff",
+    black: "#000",
+    blue: "#25a9e0",
+    darkBlue: "#06202e",
+    gray: "#888888",
+    darkGray: "#474747",
+    lightGray: "#d8d8d8",
+}
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import EmptyWrapper from '../../components/EmptyWrapper';
+import HomeContent from './Home';
+import Search from '../../components/Search';
 
 function Home (){
     return (
-        <EmptyWrapper>
-            <h1>Home</h1>
-        </EmptyWrapper>
+        <HomeContent>
+            <Search />
+            <EmptyWrapper>
+                <h1>Home</h1>
+            </EmptyWrapper>
+        </HomeContent>
     );
 }
 
