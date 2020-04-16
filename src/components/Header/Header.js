777ae4e4bfc428fd2136403483ac22093bac46fe
Andres Posada Llano <posadallano2@gmail.com>
2020-04-16 17:43:14 -0500

    Search component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,17 +3,15 @@ import { desktop, maxTablet } from "../../utils/media";
 
 const HeaderContainer = styled.header`
   ${desktop(css`
-    background-image: linear-gradient(0,rgba(51,51,51,0) 0%,rgba(51,51,51,0.52941) 46%,rgba(51,51,51,0.8) 100%);
+    background-color: #ccc;
     height: 150px;
   `)};
   ${maxTablet(css`
     background-color: #000;
     height: initial;
   `)};
-  position: fixed;
-  top: 0;
+  position: relative;
   width: 100%;
-  z-index: 999;
 `;
 
 export default HeaderContainer;
