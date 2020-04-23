4619d62ae661301bbc49f6a947310fadda8fb8a3
Andres Posada Llano <posadallano2@gmail.com>
2020-04-23 18:16:32 -0500

    One Table

--- src/views/Latest/index.js
+++ src/views/Latest/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import EmptyWrapper from '../../components/EmptyWrapper';
+import LatestContent from './Latest';
+import OneTable from '../../components/OneTable';
 
 function Latest (){
     return (
-        <EmptyWrapper>
-            <h1>Latest</h1>
-        </EmptyWrapper>
+        <LatestContent>
+            <div className="one-table-container">
+                <OneTable />
+            </div>
+            <EmptyWrapper>
+                <h1>Latest</h1>
+            </EmptyWrapper>
+        </LatestContent>
     );
 }
 
