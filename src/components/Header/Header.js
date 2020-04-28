e5d7b798c38a71f30bd1d092de1e1d0188b6b42f
Andres Posada Llano <posadallano2@gmail.com>
2020-04-28 13:10:06 -0500

    Desktop Header 60%

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import styled, { css } from "styled-components";
 import { desktop, maxTablet } from "../../utils/media";
+import { colors } from '../../utils/colors';
 
 const HeaderContainer = styled.header`
   ${desktop(css`
-    background-color: #ccc;
-    height: 150px;
+    background-color: ${colors.white};
+    height: 120px;
   `)};
   ${maxTablet(css`
     background-color: #000;
