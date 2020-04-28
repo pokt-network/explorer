8c9798470fb59dcd918f7839f1c9cd8ce7608074
Andres Posada Llano <posadallano2@gmail.com>
2020-04-28 13:10:06 -0500

    Desktop Header 60%

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -5,16 +5,22 @@ import StyledLink from "./Link";
 
 const Brand = styled(StyledLink)`
   margin-right: auto;
-  font-size: 32px;
-  font-weight: 400;
   padding: 11px 14px 11px 0;
   ${maxTablet(css`
-    font-size: 23px;
     padding: 0 14px;
   `)};
 
-  &:hover {
-    color: #ccc;
+  img {
+    max-width: 100px;
+  }
+  span {
+    display: inline-block;
+    vertical-align: top;
+    font-weight: 300;
+    font-size: 14px;
+    color: #515151;
+    margin-top: 17px;
+    margin-left: 4px;
   }
 `;
 
---
+++ src/components/Header/Ul.js
@@ -0,0 +1,11 @@
+import styled, { css } from "styled-components";
+/* import { desktop } from "../../utils/media";
+import { colors } from '../../utils/colors'; */
+
+const StyledUl = styled.ul`
+  position: relative;
+  list-style-type: none;
+  padding-left: 0;
+`;
+
+export default StyledUl;
