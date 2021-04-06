71b4be0798c4456e9ebebd11d2bcb36a60e7d262
Hamza Ouaghad <ouaghad.hamza@gmail.com>
2021-04-06 16:15:56 +0100

    chore(wip): add remaining endpoints

--- src/datasource/datasource.js
+++ src/datasource/datasource.js
@@ -1,337 +1,218 @@
 /* global BigInt */
 
-import config from "../config/config.json"
 import JSBI from 'jsbi'
 import numeral from 'numeral'
 import {
-    Pocket,
-    PocketRpcProvider,
-    HttpRpcProvider,
-    Configuration,
-    typeGuard,
-    RpcError,
-    PocketAAT,
-    StakingStatus,
-    JailedStatus
+  Pocket,
+  typeGuard,
+  RpcError,
+  StakingStatus,
+  JailedStatus
 } from "@pokt-network/pocket-js/dist/web.js"
-import { Account, Transaction, Block } from "../models"
-import { OCAlert } from '@opuscapita/react-alerts'
-import { getGatewayClient } from "./gateway";
-
-const CONFIGURATION = new Configuration(
-    config.MAX_DISPATCHERS,
-    1000,
-    0,
-    20000,
-    undefined,
-    undefined,
-    Number(config.BLOCK_TIME),
-    undefined,
-    false,
-    false
-);
-
-/**
- * Retrieve a list of URL's from the configuration for the dispatchers
- *
- * @returns {URL[]} Dispatcher urls.
- */
-function getPocketDispatchers() {
-    const dispatchersStr = config.DISPATCHERS || "";
-
-    if (dispatchersStr === "") {
-        return [];
-    }
-
-    return dispatchersStr.split(",").map(function (dispatcherURLStr) {
-        return new URL(dispatcherURLStr);
-    });
-}
-
-/**
- * @returns {HttpRpcProvider | PocketRpcProvider} RPC Provider.
- */
-async function getRPCProvider() {
-    const providerType = config.PROVIDER_TYPE;
-
-    if (providerType.toLowerCase() === "http") {
-        return getHttpRPCProvider();
-    } else if (providerType.toLowerCase() === "pocket") {
-        return await getPocketRPCProvider();
-    } else {
-        // Default to HTTP RPC Provider
-        return getHttpRPCProvider();
-    }
-}
-
-/**
- * @returns {HttpRpcProvider} HTTP RPC Provider.
- */
-function getHttpRPCProvider() {
-    const node = config.HTTP_PROVIDER;
-
-    if (!node || node === "") {
-        throw new Error(`Invalid HTTP Provider node: ${node}`);
-    }
-
-    return new HttpRpcProvider(new URL(node));
-}
-
-/**
- * @returns {PocketRpcProvider} Pocket RPC Provider.
- */
-async function getPocketRPCProvider() {    
-    const clientPrivateKey = config.CLIENT_PRIVATE_KEY;
-    const clientPassphrase = config.CLIENT_PASSPHRASE;
-    const leifAppPublicKey = config.WALLET_APP_PUBLIC_KEY;
-    const leifAppAATSignature = config.WALLET_APP_AAT_SIGNATURE;
-    const blockchain = config.CHAIN;
-
-    if (
-        clientPrivateKey &&
-        clientPassphrase &&
-        leifAppPublicKey &&
-        leifAppAATSignature &&
-        blockchain
-    ) {
-        // Dispatcher
-        const dispatchers = getPocketDispatchers();
-
-        if (!dispatchers || dispatchers.length === 0) {
-            throw new Error(`Failed to retrieve a list of dispatcher for the PocketRpcProvider: ${dispatchers}`);
-        }
-
-        // Pocket instance
-        const pocket = new Pocket(dispatchers, undefined, CONFIGURATION);
-
-        // Import client Account
-        const clientAccountOrError = await pocket.keybase.importAccount(
-            Buffer.from(clientPrivateKey, "hex"),
-            clientPassphrase
-        );
-
-        if (typeGuard(clientAccountOrError, Error)) {
-            throw clientAccountOrError;
-        };
-        
-        const clientPubKeyHex = clientAccountOrError.publicKey.toString("hex");
-
-        // Unlock the client account
-        const unlockOrError = await pocket.keybase.unlockAccount(
-            clientAccountOrError.addressHex,
-            clientPassphrase,
-            0
-        );
-
-        if (typeGuard(unlockOrError, Error)) {
-            throw clientAccountOrError;
-        };
-        
-        // Generate the AAT
-        const aat = new PocketAAT(
-            config.AAT_VERSION,
-            clientPubKeyHex,
-            leifAppPublicKey,
-            leifAppAATSignature
-        );
-
-        // Pocket Rpc Instance
-        return new PocketRpcProvider(
-            pocket,
-            aat,
-            blockchain,
-            true
-        );
-    } else {
-        throw new Error(
-            `One of the environment variables are missing: CLIENT_PRIVATE_KEY=${config.CLIENT_PRIVATE_KEY}, CLIENT_PASSPHRASE=${config.CLIENT_PASSPHRASE}, WALLET_APP_PUBLIC_KEY=${config.WALLET_APP_PUBLIC_KEY}, WALLET_APP_AAT_SIGNATURE=${config.WALLET_APP_AAT_SIGNATURE}, CHAIN=${config.CHAIN}`
-        );
-    }
-}
+import {
+  Account,
+  Transaction,
+  Block
+} from "../models"
+import {
+  OCAlert
+} from '@opuscapita/react-alerts'
+import {
+  getGatewayClient
+} from "./gateway";
+import Errors from "./errors";
 
 export class DataSource {
-    constructor(config) {
-        console.log({ config })
-        this.dispatchers = getPocketDispatchers();
-
-        if (!this.dispatchers || this.dispatchers.length === 0) {
-            throw new Error(
-                `Failed to retrieve a list of dispatchers to instantiate Pocket: ${this.dispatchers}`
-            );
-        }
-
-        this.__pocket = new Pocket(this.dispatchers, undefined, CONFIGURATION);
-        this.gatewayUrl = config ? (config.gatewayUrl || "") : "";
-        this.httpConfig = config ? (config.http || {}) : {};
-    }
-
-    /**
-     * @returns {BigInt}
-     */
-    async getHeight() {
-        const client = getGatewayClient(this.gatewayUrl, this.httpConfig);
-
-        let heightResponseOrError;
-
-        try {
-            heightResponseOrError = await client.getHeight();
-        } catch (err) {
-            OCAlert.alertError(heightResponseOrError.message, { timeOut: 3000 });
-            return undefined
-        }
-
-        return heightResponseOrError.height
-    }
-
-    /**
-     *
-     * @param {string} id
-     * @returns {Account}
-     */
-    async getAccount(id) {
-        const provider = await getRPCProvider();
-
-        const accountOrError = await this.__pocket.rpc(provider).query.getAccount(id)
-        if (typeGuard(accountOrError, RpcError)) {
-            return undefined
-        } else {
-            return new Account(accountOrError.address, (accountOrError.balance / 1000000) + " POKT", accountOrError.toJSON())
-        }
-    }
-
-    /**
-     *
-     * @param {string} id
-     */
-    async getTransaction(id) {
-        const provider = await getRPCProvider();
-        const txResponseOrError = await this.__pocket.rpc(provider).query.getTX(id)
-        if (typeGuard(txResponseOrError, RpcError)) {
-            //OCAlert.alertError(txResponseOrError.message, { timeOut: 3000 });
-            return undefined
-        } else {
-            const pocketTx = txResponseOrError.transaction
-            return new Transaction(
-                pocketTx.hash,
-                pocketTx.height,
-                undefined,
-                pocketTx.toJSON()
-            )
-        }
-    }
-
-    /**
-     *
-     * @param {string} id
-     * @returns {Block}
-     */
-    async getBlock(height) {
-        const provider = await getRPCProvider();
-        const blockResponseOrError = await this.__pocket.rpc(provider).query.getBlock(BigInt(height))
-        if (typeGuard(blockResponseOrError, RpcError)) {
-            return undefined
-        } else {
-            const block = blockResponseOrError.block
-            const blockMeta = blockResponseOrError.blockMeta
-            return new Block(
-                blockMeta.blockID.hash,
-                block.header.height.toString(),
-                block.header.time,
-                blockResponseOrError.toJSON()
-            )
-        }
-    }
+  constructor(config) {
+    const gatewayUrl = config ? (config.gatewayUrl || "") : "";
+    const httpConfig = config ? (config.http || {}) : {};
     
-    async getLatestBlock() {
-        const height = await this.getHeight()
-        if (height === undefined) {
-            return undefined
-        }
-
-        const provider = await getRPCProvider();
-        const blockResponseOrError = await this.__pocket.rpc(provider).query.getBlock(height)
-        if (typeGuard(blockResponseOrError, RpcError)) {
-            //OCAlert.alertError(blockResponseOrError.message, { timeOut: 3000 });
-            return undefined
-        } else {
-            const block = blockResponseOrError.block
-            const blockMeta = blockResponseOrError.blockMeta
-            return new Block(
-                blockMeta.blockID.hash,
-                block.header.height.toString(),
-                block.header.time,
-                blockResponseOrError.toJSON()
-            )
-        }
-
+    if (gatewayUrl === "") {
+      throw Errors.ConfigErrors.RequiredParam('gatewayUrl');
     }
 
-    /**
-     *
-     * @param {BigInt} height of the block to get the transactions from
-     * @param {number} page
-     * @param {number} perPage
-     */
-    async getLatestTransactions(page, perPage, height) {
-        const provider = await getRPCProvider();
-        const result = []
+    if (Object.keys(httpConfig).length === 0) {
+      console.warn(
+        Errors.ConfigErrors.RequiredParam('http')
+      );
+    }
 
-        const blockTxsResponseOrError = await this.__pocket.rpc(provider).query.getBlockTxs(
-            height,
-            false,
-            page,
-            perPage
+    this.gwClient = getGatewayClient(gatewayUrl, httpConfig);
+  }
+
+  /**
+   * @returns {BigInt}
+   */
+  async getHeight() {
+
+    let heightResponseOrError;
+
+    try {
+      heightResponseOrError = await this.gwClient.getHeight();
+    } catch (err) {
+      OCAlert.alertError(heightResponseOrError.message, {
+        timeOut: 3000
+      });
+      return undefined
+    }
+
+    return heightResponseOrError.height
+  }
+
+  /**
+   *
+   * @param {string} id
+   * @returns {Account}
+   */
+  async getAccount(id) {
+    const accountOrError = await this.gwClient.getAccount(id)
+
+    if (typeGuard(accountOrError, RpcError)) {
+      return undefined
+    } else {
+      return new Account(accountOrError.address, (accountOrError.balance / 1000000) + " POKT", accountOrError.toJSON())
+    }
+  }
+
+  /**
+   *
+   * @param {string} id
+   */
+  async getTransaction(id) {
+    const provider = await getRPCProvider();
+    const txResponseOrError = await this.__pocket.rpc(provider).query.getTX(id)
+    if (typeGuard(txResponseOrError, RpcError)) {
+      //OCAlert.alertError(txResponseOrError.message, { timeOut: 3000 });
+      return undefined
+    } else {
+      const pocketTx = txResponseOrError.transaction
+      return new Transaction(
+        pocketTx.hash,
+        pocketTx.height,
+        undefined,
+        pocketTx.toJSON()
+      )
+    }
+  }
+
+  /**
+   *
+   * @param {string} id
+   * @returns {Block}
+   */
+  async getBlock(height) {
+    const provider = await getRPCProvider();
+    const blockResponseOrError = await this.__pocket.rpc(provider).query.getBlock(BigInt(height))
+    if (typeGuard(blockResponseOrError, RpcError)) {
+      return undefined
+    } else {
+      const block = blockResponseOrError.block
+      const blockMeta = blockResponseOrError.blockMeta
+      return new Block(
+        blockMeta.blockID.hash,
+        block.header.height.toString(),
+        block.header.time,
+        blockResponseOrError.toJSON()
+      )
+    }
+  }
+
+  async getLatestBlock() {
+    const height = await this.getHeight()
+    if (height === undefined) {
+      return undefined
+    }
+
+    const provider = await getRPCProvider();
+    const blockResponseOrError = await this.__pocket.rpc(provider).query.getBlock(height)
+    if (typeGuard(blockResponseOrError, RpcError)) {
+      //OCAlert.alertError(blockResponseOrError.message, { timeOut: 3000 });
+      return undefined
+    } else {
+      const block = blockResponseOrError.block
+      const blockMeta = blockResponseOrError.blockMeta
+      return new Block(
+        blockMeta.blockID.hash,
+        block.header.height.toString(),
+        block.header.time,
+        blockResponseOrError.toJSON()
+      )
+    }
+
+  }
+
+  /**
+   *
+   * @param {BigInt} height of the block to get the transactions from
+   * @param {number} page
+   * @param {number} perPage
+   */
+  async getLatestTransactions(page, perPage, height) {
+    const provider = await getRPCProvider();
+    const result = []
+
+    const blockTxsResponseOrError = await this.__pocket.rpc(provider).query.getBlockTxs(
+      height,
+      false,
+      page,
+      perPage
+    )
+    if (typeGuard(blockTxsResponseOrError, RpcError)) {
+      return []
+    }
+    blockTxsResponseOrError.resultTx.forEach(element => {
+      result.push(
+        new Transaction(
+          element.hash,
+          element.height,
+          undefined,
+          element.toJSON()
         )
-        if (typeGuard(blockTxsResponseOrError, RpcError)) {
-            return []
-        }
-        blockTxsResponseOrError.resultTx.forEach(element => {
-            result.push(
-                new Transaction(
-                    element.hash,
-                    element.height,
-                    undefined,
-                    element.toJSON()
-                )
-            )
-        })
-        return result
-    }
+      )
+    })
+    return result
+  }
 
-    async getTotalStakedApps() {
-        const provider = await getRPCProvider();
-        const firstPageAppsResponseOrError = await this.__pocket.rpc(provider).query.getApps(StakingStatus.Staked, undefined, undefined, 1, 1)
-        if (typeGuard(firstPageAppsResponseOrError, RpcError)) {
-            OCAlert.alertError(firstPageAppsResponseOrError.message, { timeOut: 3000 });
-            return 0
-        } else {
-            return firstPageAppsResponseOrError.totalPages
-        }
+  async getTotalStakedApps() {
+    const provider = await getRPCProvider();
+    const firstPageAppsResponseOrError = await this.__pocket.rpc(provider).query.getApps(StakingStatus.Staked, undefined, undefined, 1, 1)
+    if (typeGuard(firstPageAppsResponseOrError, RpcError)) {
+      OCAlert.alertError(firstPageAppsResponseOrError.message, {
+        timeOut: 3000
+      });
+      return 0
+    } else {
+      return firstPageAppsResponseOrError.totalPages
     }
+  }
 
-    async getStakedSupply() {
-        const provider = await getRPCProvider();
-        const totalSupplyOrError = await this.__pocket.rpc(provider).query.getSupply()
-        if (typeGuard(totalSupplyOrError, RpcError)) {
-            OCAlert.alertError(totalSupplyOrError.message, { timeOut: 3000 });
-            return 0
-        } else {
-            const totalSupply = totalSupplyOrError
+  async getStakedSupply() {
+    const provider = await getRPCProvider();
+    const totalSupplyOrError = await this.__pocket.rpc(provider).query.getSupply()
+    if (typeGuard(totalSupplyOrError, RpcError)) {
+      OCAlert.alertError(totalSupplyOrError.message, {
+        timeOut: 3000
+      });
+      return 0
+    } else {
+      const totalSupply = totalSupplyOrError
 
-            const totalstaked = JSBI.add(totalSupply.appStaked, totalSupply.nodeStaked)
-            const totalSupplyPOKT = JSBI.divide(totalstaked, new BigInt(1000000))
-            return numeral(totalSupplyPOKT.toString()).format('(0.00 a)');
-        }
+      const totalstaked = JSBI.add(totalSupply.appStaked, totalSupply.nodeStaked)
+      const totalSupplyPOKT = JSBI.divide(totalstaked, new BigInt(1000000))
+      return numeral(totalSupplyPOKT.toString()).format('(0.00 a)');
     }
+  }
 
-    async getNodes() {
-        const provider = await getRPCProvider();
-        const validatorsResponseOrError = await this.__pocket.rpc(provider).query.getNodes(StakingStatus.Staked, JailedStatus.Unjailed, undefined, undefined, 1, 1)
-        if (typeGuard(validatorsResponseOrError, RpcError)) {
-            OCAlert.alertError(validatorsResponseOrError.message, { timeOut: 3000 });
-            return 0
-        } else {
-            return validatorsResponseOrError.totalPages
-        }
+  async getNodes() {
+    const provider = await getRPCProvider();
+    const validatorsResponseOrError = await this.__pocket.rpc(provider).query.getNodes(StakingStatus.Staked, JailedStatus.Unjailed, undefined, undefined, 1, 1)
+    if (typeGuard(validatorsResponseOrError, RpcError)) {
+      OCAlert.alertError(validatorsResponseOrError.message, {
+        timeOut: 3000
+      });
+      return 0
+    } else {
+      return validatorsResponseOrError.totalPages
     }
+  }
 }
--- src/datasource/gateway.js
+++ src/datasource/gateway.js
@@ -1,5 +1,11 @@
 import Axios from "axios";
+import Errors from "./errors";
 
+/**
+ * Http Adapter.
+ * Configure your headers and what not in here.
+ * Interceptors and Network Layer middlewares should come in here.
+ */
 class AxiosProvider {
   constructor(baseURL, config) {
     this.http = Axios.create({
@@ -10,7 +16,11 @@ class AxiosProvider {
   }
 }
 
-class PocketNetworkCtrl {
+/**
+ * Http Control Layer.
+ * Throw your http control logic in here, i.e: if error, if data, if 201.
+ */
+class PocketQueriesController {
   provider = null;
 
   requests = {
@@ -18,7 +28,68 @@ class PocketNetworkCtrl {
       url: '/v1/query/height',
       method: "post",
       data: {},
-    }
+    },
+    getAccount: (id) => ({
+      url: '/v1/query/account',
+      method: "post",
+      data: {
+        address: id,
+      },
+    }),
+    getTransaction: (id) => ({
+      url: '/v1/query/tx',
+      method: 'post',
+      data: {
+        hash: id,
+      }
+    }),
+    getBlock: (height) => ({
+      url: '/v1/query/block',
+      method: 'post',
+      data: {
+        height,
+      }
+    }),
+    getBlockTxs: (height, prove, page, per_page) => ({
+      url: '/v1/query/blocktxs',
+      method: 'post',
+      data: {
+        height,
+        page,
+        per_page,
+        prove,
+      }
+    }),
+    getApps: (staking_status, height, blockchain, page, per_page, blockchain) => ({
+      url: '/v1/query/apps',
+      method: 'post',
+      data: {
+        height,
+        staking_status,
+        page,
+        per_page,
+        blockchain,
+      }
+    }),
+    getSupply: (height) => ({
+      url: '/v1/query/supply',
+      method: 'post',
+      data: {
+        height,
+      }
+    }),
+    getNodes: (staking_status, jailed_status, height, blockchain, page, per_page) => ({
+      url: '/v1/query/nodes',
+      method: 'post',
+      data: {
+        height,
+        staking_status,
+        jailed_status,
+        blockchain,
+        page,
+        per_page,
+      }
+    }),
   }
 
   use(provider) {
@@ -32,8 +103,10 @@ class PocketNetworkCtrl {
     throw error.response.data;
   }
 
-  perform = async (requestName) => {
-    const config = this.requests[requestName];
+  perform = async (requestName, ...args) => {
+    const config = typeof this.requests[requestName] === 'function'
+      ? this.requests[requestName](...args)
+      : this.requests[requestName];
 
     const response = await this
       .provider
@@ -45,34 +118,98 @@ class PocketNetworkCtrl {
     return response;
   }
 
-  getHeight = this.perform.bind(this, 'getHeight');
+  // does not really need to be bound to this, but keeping it for semantics' sake.
+  _getHeight = this.perform.bind(this, 'getHeight');
+  _getAccount = (id) => this.perform.call(this, ['getAccount', id]);
+  _getTransaction = (id) => this.perform.call(this, ['getTransaction', id]);
+  _getBlock = this.perform.bind('getBlock', height);
+  _getBlockTxs = (height, prove, page, per_page) => this.perform.call(this, ['getBlockTxs', height, prove, page, per_page]);
+  _getApps = this.perform.bind('getApps');
+  _getSupply = this.perform.bind('getSupply');
+  _getNodes = this.perform.bind('getNodes');
 
+  // For semantic separation, and for "ease of middlewaring" when necessary.
   query = {
-    getHeight: this.getHeight,
+    getHeight: this._getHeight,
+    getAccount: this._getAccount,
+    getTransaction: this._getTransaction,
+    getBlock: this._getBlock,
+    getLatestBlock: this._getBlockTxs,
+    getApps: this._getApps,
+    getSupply: this._getSupply,
+    getNodes: this._getNodes,
   }
 }
 
+/**
+ * Exposes registered/allowed gateway queries.
+ * This layer is added for gateway level logic control.
+ */
 class GatewayClient {
   constructor(httpProvider, requestsController) {
     this.controller = requestsController.use(httpProvider)
   }
 
+  queries = [
+    'getAccount',
+    'getHeight',
+    'getTransaction',
+    'getBlock',
+    'getLatestBlock',
+    'getLatestTransactions',
+    'getTotalStakedApps',
+    'getStakedSupply',
+    'getNodes',
+  ]
+
   /**
    * @returns {BigInt}
    */
-  async getHeight() {
-    const heightResponse = await this
+  async call(queryName, ...args) {
+    if (!(this.queries.includes(queryName) > -1)) {
+      throw Errors
+    }
+    return await this
       .controller
-      .query
-      .getHeight();
+      .query[queryName](...args);
+  }
 
-    return heightResponse;
+  getHeight = this.call.bind(this, 'getHeight');
+  getAccount = this.call.bind(this, 'getAccount');
+  getTransaction = this.call.bind(this, 'getTransaction');
+  getBlock = this.call.bind(this, 'getBLock');
+
+  getLatestBlock = async () => {
+    const height = await this.getHeight();
+    const latestBlock = await this.getBlock(height);
+
+    return latestBlock;
+  }
+
+  getLatestTransactions = (page, perPage) => {
+    const height = await this.getHeight();
+    const latestTransactions = await this.getBlockTxs(height, false, 1, undefined)
+
+    return latestTransactions;
+  }
+
+  getTotalStakedApps = () => {
+    //
+  }
+
+  getStakedSupply = () => {
+    //
+  }
+
+  getNodes = () => {
+    //
   }
 }
 
+
 const getGatewayClient = (baseUrl, config) => {
   const httpProvider = new AxiosProvider(baseUrl, config);
-  const requestsCtrl = new PocketNetworkCtrl();
+  const requestsCtrl = new PocketQueriesController();
   const gwClient = new GatewayClient(httpProvider, requestsCtrl);
 
   return gwClient;
@@ -80,7 +217,7 @@ const getGatewayClient = (baseUrl, config) => {
 
 export {
   AxiosProvider,
-  PocketNetworkCtrl,
+  PocketQueriesController,
   GatewayClient,
   getGatewayClient,
 }
