747da89d7327023388bb60a45dd745ea30731649
derrandz <ouaghad.hamza@gmail.com>
2021-05-24 19:59:20 +0100

    Send NaN balance hotfix to production (#117)

--- src/views/block/latest/index.js
+++ src/views/block/latest/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import LatestContent from './latest';
 import OneTable from '../../../components/one-table';
+import { getDataSource  } from "../../../datasource";
+import { LatestInfo } from "../../../models/latestInfo";
+import config from "../../../config/config";
+
+const dataSource = getDataSource();
 
 class BlockLatest extends React.Component {
 
@@ -10,6 +15,22 @@ class BlockLatest extends React.Component {
         this.state = { 
             blocks: [] 
         };
+    }
+
+    componentWillMount() {
+        dataSource.getLatestBlock().then(block => {
+            if (block) {
+                const latest = new LatestInfo(
+                        block.id,
+                        block.number,
+                        block.timestamp,
+                        config.CHAIN_ID.toUpperCase(), //network
+                        block.data.index,
+                        undefined, 
+                    )
+              this.setState({ blocks: [latest] });
+            }
+        })
     }
 
     render() {
