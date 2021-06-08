9bb649b67477b38f2c20222ddac66f7b70ec9328
derrandz <ouaghad.hamza@gmail.com>
2021-06-08 02:08:05 +0100

    fix: substitute events with msg for transaction details (#118)
    * fix: substitute events with msg for transaction details
    
    * chore: rename object properties to a better name
    
    * chore: remove unsused constants
    
    Co-authored-by: Hamza <derrandz@Hamzas-MBP.N4>

---
+++ src/models/message.js
@@ -0,0 +1,93 @@
+const MessageTypes = {
+  AppStake: {
+    action: "AppStake",
+    keys: ["apps/MsgAppStake", "/x.apps.MsgProtoStake"],
+    actor: { name: "App", value: (msgValue) => msgValue.pubkey.value },
+    subject: { name: "Chains", value: (msgValue) => msgValue.chains },
+    object: { name: "Amount", value: (msgValue) => msgValue.amount },
+  },
+  AppUnstake: {
+    action: "AppUnstake",
+    keys: ["apps/MsgAppBeginUnstake", "/x.appsMsgBeginUnstake"],
+    actor: { name: "App", value: (msgValue) => msgValue.application_address },
+  },
+  AppUnjail: {
+    action: "AppUnjail",
+    keys: ["apps/MsgAppUnjail", "/x.nodes.MsgUnjail"],
+    actor: { name: "Node", value: (msgValue) => msgValue.address },
+  },
+  NodeStake: {
+    action: "NodeStake",
+    keys: ["pos/MsgStake", "/x.nodes.MsgProtoStake"],
+    actor: { name: "Node", value: (msgValue) => msgValue.public_key.value },
+    subject: { name: "Chains", value: (msgValue) => msgValue.chains },
+    object: { name: "Amount", value: (msgValue) => msgValue.value },
+  },
+  NodeUnstake: {
+    action: "NodeUnstake",
+    keys: ["pos/MsgBeginUnstake", "/x.nodes.MsgBeginUnstake"],
+    actor: { name: "Node", value: (msgValue) => msgValue.validator_address }
+  },
+  NodeUnjail: {
+    action: "NodeUnjail",
+    keys: ["pos/MsgUnjail", "/x.nodes.MsgUnjail"],
+    actor: { name: "Node", value: (msgValue) => msgValue.address },
+  },
+  Send: {
+    action: "Send",
+    keys: ["pos/Send", "/x.nodes.MsgSend"],
+    actor: { name: "From", value: (msgValue) => msgValue.from_address },
+    subject: { name: "To", value: (msgValue) => msgValue.to_address },
+    object: { name: "Amount", value: (msgValue) => msgValue.amount },
+  },
+  Claim: {
+    action: "Claim",
+    keys: ["pocketcore/claim"],
+    actor: { name: "Address", value: (msgValue) => msgValue.from_address },
+    subject: { name: "App", value: (msgValue) => msgValue.header.app_public_key },
+    object: { name: "Chain", value: (msgValue) => msgValue.header.chain },
+  },
+  Proof: {
+    action: "Proof/Relay",
+    keys: ["pocketcore/proof", "pocketcore/relay_proof"],
+    actor: { name: "Servicer", value: (msgValue) => msgValue.leaf.value.servicer_pub_key },
+    subject: { name: "App", value: (msgValue) => msgValue.leaf.value.aat.app_pub_key },
+    object: { name: "Blockchain", value: (msgValue) => msgValue.leaf.value.blockchain },
+  },
+}
+
+export class TxMessage {
+  constructor(msg) {
+    this.original = msg;
+    this.rendered = {}
+
+    this.render();
+  }
+
+  render() {
+    Object.keys(MessageTypes).forEach(
+      (type) => {
+        if (MessageTypes[type].keys.includes(this.original.type)) {
+          this.rendered.action = MessageTypes[type].action;
+          if (MessageTypes[type].actor) {
+            this.rendered.actor = {
+              name: MessageTypes[type].actor.name,
+              value: MessageTypes[type].actor.value(this.original.value),
+            };
+          }
+          if (MessageTypes[type].subject) {
+            this.rendered.subject = {
+              name: MessageTypes[type].subject.name,
+              value: MessageTypes[type].subject.value(this.original.value),
+            };
+          }
+          if (MessageTypes[type].object) {
+            this.rendered.object = {
+              name: MessageTypes[type].object.name,
+              value: MessageTypes[type].object.value(this.original.value),
+            };
+          }
+      }
+    })
+  }
+}
