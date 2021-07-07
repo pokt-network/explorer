446b1209e7472674fb1bf1e6fdcd0ec5857c6706
nymd <jafirmani@gmail.com>
2021-07-07 08:51:52 -0700

    Removing vestigal header

--- src/config/config.js
+++ src/config/config.js
@@ -44,7 +44,7 @@ const loadConfigFromEnv = () => {
     "GATEWAY_BASE_URL": process.env.GATEWAY_BASE_URL || "http://localhost:3000/v1/60676c9f7cbbfe002f0b9cbe",
     "HTTP_CONFIG": {
       "timeout": process.env.HTTP_TIMEOUT || 0,
-      "headers": JSON.parse(process.env.HTTP_HEADERS || '{"Content-Type": "application/json", "Blockchain-Subdomain": "mainnet"}')
+      "headers": JSON.parse(process.env.HTTP_HEADERS || '{"Content-Type": "application/json"}')
     },
     "CHAIN_ID": process.env.CHAIN_ID || "mainnet",
     "WALLET_BASE_URL": process.env.WALLET_BASE_URL,
